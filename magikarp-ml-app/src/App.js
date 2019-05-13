ecd8206a5cdc881d313b03c3dcd8ee964f814314
yasmiyazaki <kizayami.yasu@gmail.com>
2019-05-13 15:37:32 +0900

    initialized react

---
+++ magikarp-ml-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Magikarp Machine Learning App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
